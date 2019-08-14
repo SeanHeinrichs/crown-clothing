10823a8e6a8564a408fab243c7b08ff6472c2cdb
SeanHeinrichs <seanheinrichs@gmail.com>
2019-08-13 22:12:27 -0400

    added more components for sign in form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
 import Header from './components/header/header.component';
 
 // Switch: as soon as it has a match it renders it.
@@ -14,7 +16,8 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route  path='/shop' component={ShopPage}/>
+        <Route path='/shop' component={ShopPage}/>
+        <Route path='/signup' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
