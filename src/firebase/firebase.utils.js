42f492984f08dd29e6554804e488ad4dc75820db
SeanHeinrichs <seanheinrichs@gmail.com>
2019-08-25 19:50:20 -0400

    added security

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,15 +1,16 @@
 import firebase from 'firebase/app';
+import * as apiKey from './apiKey';
 import 'firebase/firestore';
 import 'firebase/auth';
 
 const config ={
-    apiKey: "AIzaSyAV8u5AvwAxU6d1GZSQkUHXe-PwIjHvPn4",
-    authDomain: "crown-db-f8e8b.firebaseapp.com",
-    databaseURL: "https://crown-db-f8e8b.firebaseio.com",
-    projectId: "crown-db-f8e8b",
-    storageBucket: "",
-    messagingSenderId: "675507589133",
-    appId: "1:675507589133:web:712e9381e41b0e80"
+    apiKey: apiKey.apiKey,
+    authDomain: apiKey.authDomain,
+    databaseURL: apiKey.databaseURL,
+    projectId: apiKey.projectId,
+    storageBucket: apiKey.storageBucket,
+    messagingSenderId: apiKey.messagingSenderId,
+    appId: apiKey.appId
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
