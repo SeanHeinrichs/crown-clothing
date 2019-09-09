c8ebda40e3a42499de9f91b4b93159565d6552fb
seanheinrichs <seanheinrichs@gmail.com>
2019-09-09 12:07:42 -0400

    started working on cart functionality

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,15 +3,17 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-import crown from '../..//assets/crown.svg';
+import { ReactComponent as Logo } from '../..//assets/crown.svg';
 
 import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
     <div className='header'>
         <Link className='logo-container' to="/">
-            <img src={crown} alt="Logo" />
+            <Logo className='logo' />
         </Link>
         <div className='options'>
             <Link className='option' to='/shop'>
@@ -20,13 +22,16 @@ const Header = ({ currentUser }) => (
             <Link className='contact' to='/contact'>
                 CONTACT
             </Link>
-            {
-                currentUser ?
-                    <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-                    :
-                    <Link className='option' to='/signup'>SIGN UP</Link>
-            }
+            {currentUser ? (
+                <div className='option' onClick={() => auth.signOut()}>
+                    SIGN OUT
+                </div>
+            ) : (
+                <Link className='option' to='/signup'>SIGN UP</Link>
+            )}
+            <CartIcon />
         </div>
+        <CartDropdown />
     </div>
 );
 
