1aafa23647a4e39101fbc1c694d383bdf6e6f86a
seanheinrichs <seanheinrichs@gmail.com>
2019-09-11 12:40:45 -0400

    fixing security vulnerabilities

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,10 +7,10 @@ import { ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ( { toggleCartHidden } ) => (
+const CartIcon = ( { toggleCartHidden, itemCount } ) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 )
 
@@ -18,8 +18,17 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
+// This is called a selected. Gets entire state and then pulls of a portion.
+// We get cartItems then reduce over it to get the quantity.
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    itemCount: cartItems.reduce(
+        (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+        0
+    )
+})
+
 export default connect(
-    null, 
+    mapStateToProps, 
     mapDispatchToProps
 )(CartIcon);
 
