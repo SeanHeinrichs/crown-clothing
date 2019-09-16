87981d07f8134dcac58dae5d9d684d545de8e46f
seanheinrichs <seanheinrichs@gmail.com>
2019-09-16 10:29:05 -0400

    temp adding back API ked info

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,13 +4,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config ={
-    apiKey: apiKey.apiKey,
-    authDomain: apiKey.authDomain,
-    databaseURL: apiKey.databaseURL,
-    projectId: apiKey.projectId,
-    storageBucket: apiKey.storageBucket,
-    messagingSenderId: apiKey.messagingSenderId,
-    appId: apiKey.appId
+    apiKey: "AIzaSyAV8u5AvwAxU6d1GZSQkUHXe-PwIjHvPn4",
+    authDomain: "crown-db-f8e8b.firebaseapp.com",
+    databaseURL: "https://crown-db-f8e8b.firebaseio.com",
+    projectId: "crown-db-f8e8b",
+    storageBucket: "",
+    messagingSenderId: "675507589133",
+    appId: "1:675507589133:web:712e9381e41b0e80"
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
